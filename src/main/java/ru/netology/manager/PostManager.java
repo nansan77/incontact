aba7b16e23e19a07a914e2c85fd5b03e181e8767
Aleksandr Nikitin <nan.alek@yandex.ru>
2021-02-14 11:47:03 +0300

    VK Post

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,18 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+
+        private Post[] posts;
+
+        public Post[] searchPost(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+
+            return null;
+        }
+
+        public void deletePost(int ownerId, int postId) {
+
+            return;
+        }
+}
